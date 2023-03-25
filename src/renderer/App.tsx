834f289b2a65a70639068d47b967c9818c3b4b6a
David Buehler <creighti@galacticnorth.net>
2023-03-25 11:58:48 -0700

    refactor: split App.css to Root.css

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,28 +1,6 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-// import Button from '@mui/material/Button';
-// import icon from '../../assets/icon.svg';
 import './App.css';
 import { Root } from './Root';
-
-// function Hello() {
-//   return (
-//     <div>
-//       <div className="Hello">
-//         <img width="200" alt="icon" src={icon} />
-//       </div>
-//       <h1>electron-react-boilerplate</h1>
-//       <div>
-//         <Button variant="contained" className="Hello">
-//           Hello World
-//         </Button>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default function App() {
   return (
--- src/renderer/Root.tsx
+++ src/renderer/Root.tsx
@@ -1,12 +1,16 @@
 import Button from '@mui/material/Button';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import icon from '../../assets/icon.svg';
-import './App.css';
+import './Root.css';
 
 // eslint-disable-next-line import/prefer-default-export
 export function Root() {
   return (
     <div>
-      <div className="Hello">
+      <div className="Root">
         <img width="200" alt="icon" src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
